ff734fb660bed137b2d7e575c382f48ee90df12b
Yaroslav-Andrii <instructormedusa@gmail.com>
2019-11-19 16:59:21 +0200

    Added request error handling

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AuthService } from './shared/auth.service';
 import { UserService } from './shared/user.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import IUser from './interfaces/user.interface';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +18,24 @@ export class AppComponent{
     this.authorization();
   }
 
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error.error);
+      return of(result as T);
+    }
+  }
+
   private async authorization(): Promise<void> {
     if (!localStorage.getItem('_access_token')) {
       this.signedIn = false;
     } else {
-      this.MyData = await this.authService.getUser().pipe(/* TODO ERROR HANDLER */).subscribe(data => {
+      this.MyData = await this.authService.getUser()
+      .pipe(
+        catchError(this.handleError<IUser>())
+      )
+      .subscribe(data => {
+        if (!data) return;
+
         this.MyData = data;
         this.userService.initialUser(data);
         this.signedIn = true;
@@ -28,10 +44,16 @@ export class AppComponent{
   }
 
   private async openMessenger(status: boolean): Promise<void> {
-    this.MyData = await this.authService.getUser().pipe(/* TODO ERROR HANDLER */).subscribe(data => {
+    this.MyData = await this.authService.getUser()
+    .pipe(
+      catchError(this.handleError<IUser>())
+    )
+    .subscribe(data => {
+      if (!data) return;
+        
       this.MyData = data;
       this.userService.initialUser(data);
+      this.signedIn = status;
     });
-    this.signedIn = status;
   }
 }
