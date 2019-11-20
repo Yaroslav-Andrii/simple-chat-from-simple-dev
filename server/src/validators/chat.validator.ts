06cf2daaa2301b49ef829677d09504b4cb71ff3d
Yaroslav-Andrii <instructormedusa@gmail.com>
2019-11-20 20:35:02 +0200

    Created caht validator

---
+++ server/src/validators/chat.validator.ts
@@ -0,0 +1,23 @@
+import Joi from '@hapi/joi';
+import IChat from '../interfaces/chat.interface';
+
+export function chatValidator(data: IChat): Joi.ValidationResult {
+
+	// Setting validation schema
+	const schema = Joi.object({
+		name: Joi.string()
+			.min(3)
+			.max(255)
+			.required(),
+		users: Joi.array()
+			.required(),
+		messages: Joi.array(),
+	});
+
+	// Validation
+	const { error, value } = schema.validate(data);
+	if (error) { 
+		throw error;
+	} 
+	return value;
+}
--- server/src/validators/index.ts
+++ server/src/validators/index.ts
@@ -1,7 +1,9 @@
-import * as user from './user.validator'
+import * as user from './user.validator';
+import * as chat from './chat.validator';
 
 const Validators = {
 	user,
+	chat,
 };
 
 export default Validators;
