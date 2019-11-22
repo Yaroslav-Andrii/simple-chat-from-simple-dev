b77c12aa5c74a67dc929114449912f8b4965283e
Yaroslav-Andrii <instructormedusa@gmail.com>
2019-11-22 16:48:53 +0200

    Added and Fixed
    1. Added, A disconnect listener to socket
    2. Fixed, Split message function on message and writeBuffer function
    3. Fixed, A typing in functions

--- server/src/controllers/web-socket.controller.ts
+++ server/src/controllers/web-socket.controller.ts
@@ -9,10 +9,14 @@ let currentRoomName: string;
 
 export function connection(socket: socketIo.Socket) {
 
-	socket.on('joinTo', async chatId => {
+	socket.on('joinTo', async (chatId: string) => {
 		try {
+			if (buffer.length) {
+				writeBuffer();
+			}
+
 			currentChat = chatId;
-			currentRoomName = await Services.chatService.getRoomNameById(chatId);
+			currentRoomName = <string>(await Services.chatService.getRoomNameById(chatId));
 
 			if (!currentRoomName) throw new Error(`Chat with id ${currentChat} not definded`);
 
@@ -22,23 +26,33 @@ export function connection(socket: socketIo.Socket) {
 		}
 	})
 
-	socket.on('message', (data) => {
+	socket.on('message', (data: IMessage) => {
 		message(data);
 		socket.broadcast.to(currentRoomName).emit('incoming', data);
 	});
+
+	socket.on('disconnect', _ => {
+		if (buffer.length) {
+			writeBuffer();
+		}
+	})
 }
 
-function message(data: IMessage, flag: boolean = false) {
+function message(data: IMessage) {
 	try {
-		if (buffer.length >= 100 || flag) {
-			Services.chatService.pushMessages(buffer, currentChat);
-			console.log(buffer);
-			buffer = [];
-			console.log(buffer)
-			return;
-		}
+		if (buffer.length >= 100) writeBuffer();
+
 		buffer.push(data);
 		return;
+	} catch (error) {
+		
+	}
+}
+
+function writeBuffer(): void {
+	try {
+		Services.chatService.pushMessages(buffer, currentChat);
+		buffer = [];
 	} catch (error) {
 		console.error(error.message);
 	}
