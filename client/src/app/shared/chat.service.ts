98bb2dff42572f9dba5b6c28bf631fa987b5763e
Yaroslav-Andrii <instructormedusa@gmail.com>
2019-11-28 17:12:47 +0200

    Added and Fixed
    1. Added public method 'getMessagesByChatId'
    2. Fixed public method 'joinTo'
    3. Fixed public method 'send'
    4. Fixed socket emit function 'incoming'
    5. Fixed call of socket 'initializeUser' function

--- client/src/app/shared/chat.service.ts
+++ client/src/app/shared/chat.service.ts
@@ -4,6 +4,7 @@ import IChat from '../interfaces/chat.interface';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import IMessage from '../interfaces/message.interface';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,19 +14,28 @@ export class ChatService {
   private url = 'http://localhost:3000';
   private socket: SocketIOClient.Socket; 
   private ownId: string;
+  private messagesLength: number = 10;
+  private currentChatId: string;
 
   public incomingMessage: EventEmitter<IMessage> = new EventEmitter();
   public chatActivated: EventEmitter<IChat> = new EventEmitter();
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private userService: UserService
   ) {
-
+    this.ownId = userService.getOwnId();
+    
     // Connect to web socket
     this.socket = io(this.url);
+    this.socket.emit('initializeUser', this.ownId);
 
-    this.socket.on('incoming', data => {
-      this.incomingMessage.emit(data);
+    this.socket.on('incoming', (data: IMessage, chatId: string) => {
+      if (this.currentChatId === chatId) {
+        this.incomingMessage.emit(data);
+      } else {
+        // TODO show new messages in title
+      }
     })
   }
 
@@ -34,10 +44,15 @@ export class ChatService {
   }
 
   public joinTo(id) {
+    this.currentChatId = id;
     this.socket.emit('joinTo', id);
   }
 
   public send(message) {
-    this.socket.emit('message', message);
+    this.socket.emit('message', message, this.currentChatId);
+  }
+
+  public getMessagesByChatId(id): Observable<IMessage[]> {
+    return this.httpClient.get<IMessage[]>(`${this.url}/chats/${id}/messages?length=${this.messagesLength}`);
   }
 }
