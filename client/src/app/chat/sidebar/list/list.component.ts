f3f4a2ae448816ad162d83affbea967c690c8777
Yaroslav-Andrii <instructormedusa@gmail.com>
2019-12-04 17:52:22 +0200

    Renamed and Fixed
    1. Renamed name of private method 'getChatList' to 'getLinkList'
    2. Fixed logic of list rendering, searching and sharing

--- client/src/app/chat/sidebar/list/list.component.ts
+++ client/src/app/chat/sidebar/list/list.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import IChat from 'src/app/interfaces/chat.interface';
 import { ChatService } from 'src/app/shared/chat.service';
 import ISearchParams from 'src/app/interfaces/search-params.interface';
+import IUserFriend from 'src/app/interfaces/user-friend.interface';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-list',
@@ -14,34 +16,61 @@ export class ListComponent implements OnInit {
   @Input() searchParams: ISearchParams;
 
   private defaultSearchParams: ISearchParams = { 
-    flag: 'public',
-    string: '' 
+    type: 'public',
+    string: '',
+    deepResult: null,
   }
 
   private chatsList: IChat[];
   private active: string;
+  private friendList: IUserFriend[];
 
-  constructor(private chatService: ChatService ) {
+  constructor(
+    private chatService: ChatService,
+    private userService: UserService
+  ) {
     this.chatService.loadGroupChatsList()
       .subscribe(data => {
         this.chatsList = data;
       });
+
+    this.friendList = this.userService.getFriends();
   }
 
-  private getChatList(searchParams: ISearchParams = this.defaultSearchParams): IChat[] {
+  private getLinkList(searchParams: ISearchParams = this.defaultSearchParams): IChat[] | IUserFriend[] {
     
-    if (!this.chatsList) return;
+    if (searchParams.type === 'public') {
+      if (!this.chatsList) return;
 
-    const chats = this.chatsList.filter(chat => chat.type === searchParams.flag);
+      const chats = this.chatsList.filter(chat => chat.type === searchParams.type);
 
-    return chats.filter(chat => {
-      return chat.name.toLowerCase().includes( searchParams.string.trim().toLowerCase() );
-    });
+      return chats.filter(chat => {
+        return chat.name.toLowerCase().includes( searchParams.string.trim().toLowerCase() );
+      });
+    } 
+
+    const friends: IUserFriend[] = this.friendList.filter(friend => friend.name === searchParams.string);
+
+    if (searchParams.deepResult) {
+      friends.push(...searchParams.deepResult);
+      return friends;
+    }
+
+    return friends
   }
 
   public activate(id: string): void {
-    this.chatService.chatActivated.emit( this.chatsList.find(chat => chat._id === id) );
+
+    if (this.searchParams && this.searchParams.type === 'public') {
+      this.chatService.chatActivated.emit( this.chatsList.find(chat => chat._id === id) );
+    } else {
+      // TODO private activating
+      console.log('pivate activate');
+    }
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    console.log(this.active);
+    
+   }
 }
