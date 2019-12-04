421a5b47442adc1686c6a3e1b9ae87da8a2ed65f
Yaroslav-Andrii <instructormedusa@gmail.com>
2019-12-04 17:48:08 +0200

    Added method 'getUsers'

--- client/src/app/shared/user.service.ts
+++ client/src/app/shared/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import IUserFriend from '../interfaces/user-friend.interface';
 import IUser from '../interfaces/user.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,11 @@ import IUser from '../interfaces/user.interface';
 export class UserService {
 
   private UserInfo: IUser;
+  private url = 'http://localhost:3000';
 
-  constructor() {}
+  constructor(
+    private httpClient: HttpClient
+  ) {}
 
   public initialUser(data: IUser) {
     this.UserInfo = data;
@@ -19,8 +24,12 @@ export class UserService {
     return this.UserInfo.friends;
   }
 
+  public getUsers(searchString: string): Observable<IUserFriend[]> {
+    return this.httpClient.get<IUserFriend[]>(`${this.url}/users?searchString=${searchString}`);
+  }
+
   public getOwnId(): string {
-    return this.UserInfo.id;
+    return this.UserInfo._id;
   }
 
   public getOwnName(): string {
