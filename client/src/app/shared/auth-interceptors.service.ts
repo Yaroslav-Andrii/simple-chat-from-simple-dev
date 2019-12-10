1f8e84cc37d64a3ef903a903240311525c6215a4
Yaroslav-Andrii <instructormedusa@gmail.com>
2019-12-10 18:21:20 +0200

    Fixed work logic on no has access token situation

--- client/src/app/shared/auth-interceptors.service.ts
+++ client/src/app/shared/auth-interceptors.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpEvent, HttpHandler } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,14 @@ export class AuthInterceptors implements HttpInterceptor{
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     
-    if (!localStorage.getItem('_access_token')) return next.handle(req);
+    if (!localStorage.getItem('_access_token')) {
+      this._router.navigate(['/login']);
+      return next.handle(req);
+    } 
 
     const request = req.clone({setHeaders: {'access-token': localStorage.getItem('_access_token')}});
     return next.handle(request)
   }
 
-  constructor() { }
+  constructor(private _router: Router) { }
 }
