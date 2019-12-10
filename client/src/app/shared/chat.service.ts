cf0f7a4aa906c1f6f3c7971aaf2efc8109c3aa07
Yaroslav-Andrii <instructormedusa@gmail.com>
2019-12-10 18:18:14 +0200

    Added and Fixed
    1. Added public method 'findUsersByParams'
    2. Added public method 'getChatById'
    3. Added public method 'createPrivateChat'
    4. Fixed style mistakes

--- client/src/app/shared/chat.service.ts
+++ client/src/app/shared/chat.service.ts
@@ -5,6 +5,8 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import IMessage from '../interfaces/message.interface';
 import { UserService } from './user.service';
+import IUserFriend from '../interfaces/user-friend.interface';
+import ISearchParams from '../interfaces/search-params.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -33,24 +35,22 @@ export class ChatService {
 
     this.socket.on('joined', flag => {
       this.chatJoined.emit(flag);
-    })
+    });
 
     this.socket.on('incoming', (data: IMessage, chatId: string) => {
       if (this.currentChatId === chatId) {
         this.incomingMessage.emit(data);
-      } else {
-        // TODO show new messages in title
       }
-    })
+    });
   }
 
-  public loadGroupChatsList(): Observable<IChat[]> {
+  public loadPublicChatsList(): Observable<IChat[]> {
     return this.httpClient.get<IChat[]>(`${this.url}/chats`)
   }
 
-  public joinTo(id) {
-    this.currentChatId = id;
-    this.socket.emit('joinTo', id);
+  public joinTo(chatId) {
+    this.currentChatId = chatId;
+    this.socket.emit('joinTo', chatId);
   }
 
   public send(message) {
@@ -59,5 +59,23 @@ export class ChatService {
 
   public getMessagesByChatId(id): Observable<IMessage[]> {
     return this.httpClient.get<IMessage[]>(`${this.url}/chats/${id}/messages?length=${this.messagesLength}`);
+  }
+
+  public findUsersByParams(searchParams: ISearchParams): Observable<IUserFriend[]> {
+    return this.httpClient.get<IUserFriend[]>(`${this.url}/users?string=${searchParams.string}`);
+  }
+
+  public getChatById(id: string): Observable<IChat> {
+    return this.httpClient.get<IChat>(`${this.url}/chats/${id}`);
+  }
+
+  public createPrivateChat(ownId: string, friendId: string): Observable<IChat> {
+    return this.httpClient.post<IChat>(
+      `${this.url}/chats/createNew`, 
+      {
+        users: [ownId, friendId],  
+        type: 'friend'
+      }
+    )
   }
 }
