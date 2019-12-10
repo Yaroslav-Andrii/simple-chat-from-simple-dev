0ffc4a0b781a558c3350bf97864e5c59c1e55d50
Yaroslav-Andrii <instructormedusa@gmail.com>
2019-12-10 18:01:31 +0200

    Fixed logic of 'chats' router. Now it send only public chats!

--- server/src/routes/chats.route.ts
+++ server/src/routes/chats.route.ts
@@ -7,7 +7,7 @@ const chatsRouter = express.Router();
 
 chatsRouter.get('/', Middlewares.auth, async(req: express.Request, res: express.Response) => {
 	const roomsList: IChat[] = await Services.chatService.getAllChats();
-	res.send(roomsList);
+	res.send( roomsList.filter(room => room.type === 'public') );
 });
 
 chatsRouter.get('/:id', Middlewares.auth, async(req: express.Request, res: express.Response) => {
