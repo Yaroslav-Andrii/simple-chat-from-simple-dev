2ec2933d3ca19108355f3876eaf1b5421a55c9f4
Yaroslav-Andrii <instructormedusa@gmail.com>
2019-12-10 18:12:41 +0200

    Added TMP System routes for development and debugging

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,7 @@ import http from 'http';
 import socketIo from 'socket.io';
 import routes from './routes';
 
-import Controllers from './controllers';
+import {socketController} from './controllers';
 
 // Get environment data
 import dotenv from 'dotenv';
@@ -11,6 +11,9 @@ dotenv.config();
 
 // Connect to database
 import * as database from './database';
+import Services from './services';
+import IChat from './interfaces/chat.interface';
+
 database.setUpConnection();
 
 // Start app
@@ -21,14 +24,14 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 // Web socket connection
-io.on('connection', Controllers.socket.connection);
+io.on('connection', socketController);
 
 // Setting body parsers
 app.use( express.json() );
-app.use( express.urlencoded({ extended: true }));
+app.use( express.urlencoded({ extended: true }) );
 
-// CROSS
-if (process.argv.includes('cross')) {
+// CORS settings
+if ( process.argv.includes('cors') ) {
 	app.use('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
 
 		res.set({
@@ -46,9 +49,40 @@ if (process.argv.includes('cross')) {
 app.use('/register', routes.register);
 app.use('/login', routes.login);
 app.use('/getUserInfo', routes.getUserInfo)
-
 app.use('/chats', routes.chats);
-app.use('/users', routes.contacts);
+app.use('/users', routes.users);
+
+/* TMP System routes for development and debugging */
+app.get('/clear', async (req: express.Request, res: express.Response) => {
+
+	await Services.chatService.remove();
+	await Services.userService.remove();
+
+	res.send('ok');
+});
+
+app.get('/check', async (req: express.Request, res: express.Response) => {
+
+	const response = [];
+
+	response.push( await Services.chatService.getAllChats() );
+	response.push( await Services.userService.getAll() );
+
+	res.send(response);
+});
+
+app.get('/basechat', async (req: express.Request, res: express.Response) => {
+
+	const chat: IChat = {
+		name: req.query.name || `Chat ${Math.round(Math.random() * 100)}`,
+		users: [],
+		messages: [],
+		type: 'public',
+	}
+
+	await Services.chatService.createChat(chat);
+	res.send('ok');
+});
 
 // Set listening port 
 server.listen(process.env.SERVER_PORT, () => {
